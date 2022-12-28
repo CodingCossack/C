3e8c15b361f03ed29cdfe0bc0292340a7955c91d
Dima <108333654+CodingCossack@users.noreply.github.com>
2022-12-28 00:13:46 +0000

    Add files via upload

---
+++ Learning/3. Strings/copyString-without-strcpy().c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int int main()
+{
+	char source [100];
+	char destination [100];
+
+	int i;
+
+	printf("Enter the source string: ");
+	fgets(source, sizeof(source));
+
+	for(i = 0; i strlen(source); i++){
+		destination[i] = source[i]
+
+	}
+
+	destination[i] = '\0';
+	printf("%s", destination);
+
+	return 0;
+}
