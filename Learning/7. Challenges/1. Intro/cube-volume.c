667b018d3fd2ea1497f8257513bad93da1ee5457
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-01-02 17:17:54 +0000

    Add files via upload

---
+++ Learning/7. Challenges/1. Intro/cube-volume.c
@@ -0,0 +1,16 @@
+//find volume of cube
+
+#include <stdio.h>
+
+int main() {
+
+    int length;
+
+    scanf("%d", &length);
+
+    int volume = length * length * length;
+
+    printf("%d", volume);
+
+    return 0;
+}
