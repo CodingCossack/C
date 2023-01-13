875cf3d58e0da4d0458e7391b22d01568a9037cb
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-01-13 09:47:41 +0000

    Add files via upload

---
+++ Learning/7. Challenges/2. Control Flow/print-3-sentences.c
@@ -0,0 +1,12 @@
+// Program to print a sentence 3 times using a loop.
+
+#include <stdio.h>
+
+int main() {
+    
+    for(int i = 0; i < 3; i++) {				// run a for loop from i = 0 to i < 3
+        printf("C Programming is fun\n");
+    }
+
+    return 0;
+}
